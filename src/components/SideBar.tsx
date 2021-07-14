80e770056fba8ec0ea6c9fe0ecec4f5d1748855c
Karina Borges <karina@cuponeria.com.br>
2021-07-14 10:38:47 -0300

    ✨ Challenge completed

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,40 @@
-export function SideBar() {
+import { Button } from "./Button";
+
+interface GenreProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+interface GenreResponseProps {
+  genres: GenreProps[];
+  selectedGenreId: number;
+  handleClick: (id: number) => void;
+}
+
+export function SideBar({
+  genres,
+  selectedGenreId,
+  handleClick,
+}: GenreResponseProps): JSX.Element {
   // Complete aqui
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClick(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
